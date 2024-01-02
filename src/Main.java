3b104e0e292decefeeca7518f850dca58a6fc719
Lindokuhle Mashele <lmashele023b@student.wethinkcode.co.za>
2024-01-02 17:14:29 +0200

    url

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+//    https://api.sunrisesunset.io/json?lat=30.5595&lng=22.9375
+}
