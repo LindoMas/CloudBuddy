496e609ba11b5d1d47cf3d46698d567311cc9091
Lindokuhle Mashele <lmashele023b@student.wethinkcode.co.za>
2024-01-03 07:27:03 +0200

    Finally!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+/*
+/   Powered by SunriseSunset.io
+ */
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public Main() {
     }
-//    https://api.sunrisesunset.io/json?lat=30.5595&lng=22.9375
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello world!");
+        String urlLink="https://api.sunrisesunset.io/json?lat=30.5595&lng=22.9375/json?lat=30.5595&lng=22.9375";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =  HttpRequest.newBuilder().GET().uri(URI.create(urlLink)).build();
+
+        HttpResponse <String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+//    https://api.sunrisesunset.io/json?lat=30.5595&lng=22.9375/json?lat=30.5595&lng=22.9375
+
+
 }
